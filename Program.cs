b5a26d5ca3c137d351e15bd7b8245c8d13af1558
Wrdouuu <164611150+Wrdouuu@users.noreply.github.com>
2024-08-12 12:42:55 +0700

    hi

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿﻿using System;
+
+namespace DEBUG
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Index = " + i);
+            }
+
+        }
+    }
+}
